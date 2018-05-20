aed9854a654526f0adf4f1c750c8cc9b0e20901c
opronhan <37853586+opronhan@users.noreply.github.com>
2018-05-20 21:25:16 +0700

    Thêm enemy và tạo database cho game
    Tạo hình dạng cho kẻ thù
    Thêm animation và chức năng cho kẻ thù
    Tạo database và setup database cho unity

---
+++ Assets/Assets/Scripts/Enemy script/JumpScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour {
+    public bool grounded;
+    public Transform groundcheck;
+    private Rigidbody2D rb;
+    protected Animator animator;
+    public Vector3 jumpForce;
+    public LayerMask charmask;
+    // Use this for initialization
+    void Start () {
+        animator = gameObject.GetComponent<Animator>();
+        jumpForce = new Vector3(0, 8, 0);
+        groundcheck = GameObject.Find("enemygroundcheck").GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, 0.5f, charmask);
+        jump();
+    }
+    void jump()
+    {
+        if (grounded)
+        {
+            rb.velocity = jumpForce;
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/Enemy script/WalkScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkScript : MonoBehaviour {
+
+    public float speed = 1f;
+    private Rigidbody2D rb;
+    protected Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+    // Use this for initialization
+    void Start () {
+        animator = gameObject.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        animator.SetBool("Walk", true);
+        Move();
+	}
+
+    void Move()
+    {
+        rb.AddForce(new Vector2(transform.localScale.x, 0) * speed);
+    }
+}
